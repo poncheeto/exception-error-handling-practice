66133c2a6eb283a99b6622826e131e6beddc6522
Poncho <jplyles1212@gmail.com>
2020-10-17 19:03:12 +0900

    Add more code to practice

--- ex_test.rb
+++ ex_test.rb
@@ -1,1 +1,10 @@
-puts "Hello, world"
+while 1
+    puts 'Enter a number>>'
+    begin
+        num = Kernel.gets.match(/\d+/)[0]
+    rescue
+        puts "Erroneus input! Try again..."
+    else
+        puts "#{num} + 1 is: #{num.to_i + 1}"
+    end
+end
