dc83deb3ea108a342232934a81fc23654d768128
Poncho <jplyles1212@gmail.com>
2020-10-18 13:42:02 +0900

    Add changes to make rubocop happy

--- ex_test.rb
+++ ex_test.rb
@@ -1,12 +1,12 @@
 while 1
-    puts 'Enter a number>>'
-    begin
-        num = Kernel.gets.match(/\d+/)[0]
-    rescue Exception=>e
-        puts "Erroneus input!"
-        puts e
-        puts "\tTry again...\n"
-    else
-        puts "#{num} + 1 is: #{num.to_i + 1}"
-    end
+  puts 'Enter a number>>'
+  begin
+    num = Kernel.gets.match(/\d+/)[0]
+  rescue Exception=>e
+    puts 'Erroneus input!'
+    puts e
+    puts "\tTry again...\n"
+  else
+    puts "#{num} + 1 is: #{num.to_i + 1}"
+  end
 end
